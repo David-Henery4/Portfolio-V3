58274b67d673ec8ccb08d4072d90b8ab5a4f196f
DizzlyD <david4henery00@hotmail.com>
2025-04-02 21:22:14 +0100

    added-mobile-nav

---
+++ src/local-data/navdata.ts
@@ -0,0 +1,31 @@
+
+
+const navdata = [
+  {
+    id: 1,
+    pageName: "Home",
+    route: "/"
+  },
+  {
+    id: 2,
+    pageName: "About",
+    route: "/about"
+  },
+  {
+    id: 3,
+    pageName: "Projects",
+    route: "/projects"
+  },
+  {
+    id: 4,
+    pageName: "Skills",
+    route: "/skills"
+  },
+  {
+    id: 5,
+    pageName: "contact",
+    route: "/contact"
+  },
+]
+
+export default navdata
