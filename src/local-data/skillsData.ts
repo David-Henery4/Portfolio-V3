b74e92943e4e6d17f993bf8138fbf23d17cc35a8
DizzlyD <david4henery00@hotmail.com>
2025-04-07 22:54:31 +0100

    added-skills-grid

---
+++ src/local-data/skillsData.ts
@@ -0,0 +1,43 @@
+
+
+
+const skillsData = [
+  {
+    id: 456745,
+    name: "HTML",
+    value: "html",
+    icon: "",
+  },
+  {
+    id: 45674556546,
+    name: "CSS",
+    value: "css",
+    icon: "",
+  },
+  {
+    id: 4567454758523,
+    name: "Javascript",
+    value: "javascript",
+    icon: "",
+  },
+  {
+    id: 4534654798,
+    name: "React",
+    value: "react",
+    icon: "",
+  },
+  {
+    id: 9246,
+    name: "NextJS",
+    value: "nextjs",
+    icon: "",
+  },
+  {
+    id: 49782136767,
+    name: "TailwindCSS",
+    value: "tailwindcss",
+    icon: "",
+  },
+]
+
+export default skillsData
