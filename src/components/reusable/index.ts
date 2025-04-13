f3b8ef0c00f5523299d1605eb9957b450f502330
DizzlyD <david4henery00@hotmail.com>
2025-04-13 20:12:07 +0100

    reusable-nav-links-&-socials

--- src/components/reusable/index.ts
+++ src/components/reusable/index.ts
@@ -1,7 +1,11 @@
 import PrimaryButton from "./PrimaryButton.astro";
 import SecondaryButton from "./SecondaryButton.astro";
+import Socials from "./Socials.astro";
+import Navigation from "./Navigation.astro";
 
 export {
   PrimaryButton,
-  SecondaryButton
+  SecondaryButton,
+  Socials,
+  Navigation,
 }
