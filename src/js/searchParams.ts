6b57a700e446181021416fd5015048520a032950
DizzlyD <david4henery00@hotmail.com>
2025-05-12 22:52:55 +0100

    fetching-filtered-data-from-searchParams

--- src/js/searchParams.ts
+++ src/js/searchParams.ts
@@ -1,36 +1,46 @@
+
+
 const filtersAndPillContainer = document.querySelector(".filters") as HTMLDivElement;
+import { navigate } from "astro:transitions/client";
 
 
 filtersAndPillContainer?.addEventListener("click", (e) => {
+  
   const EVtarget = e.target as HTMLElement
   const btnElement = EVtarget.closest("button");
+  
   if (btnElement && btnElement.dataset?.category) {
-    btnElement.dataset?.category;
-    console.log("Category", btnElement.dataset.category);
+    
     const newUrl = createQueryString("name", btnElement.dataset?.category);
-    console.log("newUrl: ", newUrl);
-    history.pushState({}, "", `?${newUrl}`)
+    window.history.pushState({}, "", `?${newUrl}`)
+    navigate(`/projects?${newUrl}`)
   }
+  
 });
 
+
 const createQueryString = (key: string, value: string) => {
+  
   const currentParams = window.location.search
-  console.log("CurrentParams: ", currentParams)
   const params = new URLSearchParams(currentParams);
-  console.log("Params: ", params);
   const previousValues = params.get(key);
   const combinedValues = `${previousValues},${value}`;
-  console.log("CombinedValues", combinedValues)
+  
+  
   if (!previousValues) {
     params.set(key, value);
     console.log("called: ", params.toString())
     return params.toString();
   }
+  
+  
   if (previousValues.includes(value)) {
+
     const combinedValueArray = combinedValues
       .split(",")
       .filter((item) => item !== value);
-    console.log("combinedArray:", combinedValueArray);
+    
+    
     if (combinedValueArray.length) {
       params.delete(key);
       return params.toString();
@@ -38,6 +48,10 @@ const createQueryString = (key: string, value: string) => {
     params.set(key, combinedValues);
     return params.toString();
   }
+  
+  
   params.set(key, combinedValues);
   return params.toString();
+  
 };
+
--- src/queries/getProjects.ts
+++ src/queries/getProjects.ts
@@ -1,11 +1,31 @@
 import { stringify } from "qs-esm";
+import type { ProjectType } from "src/types/projects-types";
 
-const getProjects = async (queries: string[]) => {
-  const stringifiedQuery = stringify(
-    {
-      where: {
-        
+const getProjects = async (queries?: string[], isFiltered: boolean = false) => {
+
+  if (isFiltered){
+    const stringifiedQuery = stringify(
+      {
+        where: {
+          "tags.category": {
+            in: queries,
+          }
+        },
+      },
+      {
+        addQueryPrefix: true
       }
-    }
-  )
+    )
+    const res = await fetch(`http://localhost:8000/api/projects${stringifiedQuery}`);
+    const { docs } = await res.json();
+    const projectData: ProjectType[] = docs;
+    return projectData
+  }
+  
+  const res = await fetch("http://localhost:8000/api/projects");
+  const { docs } = await res.json();
+  const projectData: ProjectType[] = docs;
+  return projectData;
 }
+
+export default getProjects
