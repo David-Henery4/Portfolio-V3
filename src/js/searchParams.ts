4c71706a7b51ebbfc8bcc478228792e1a945cefa
DizzlyD <david4henery00@hotmail.com>
2025-05-13 14:33:43 +0100

    searchParams-filters-functional

--- src/js/searchParams.ts
+++ src/js/searchParams.ts
@@ -1,57 +1,47 @@
-
-
-const filtersAndPillContainer = document.querySelector(".filters") as HTMLDivElement;
+const filtersAndPillContainer = document.querySelector(
+  ".filters"
+) as HTMLDivElement;
 import { navigate } from "astro:transitions/client";
 
-
 filtersAndPillContainer?.addEventListener("click", (e) => {
-  
-  const EVtarget = e.target as HTMLElement
+  const EVtarget = e.target as HTMLElement;
   const btnElement = EVtarget.closest("button");
-  
+
   if (btnElement && btnElement.dataset?.category) {
-    
     const newUrl = createQueryString("name", btnElement.dataset?.category);
-    window.history.pushState({}, "", `?${newUrl}`)
-    navigate(`/projects?${newUrl}`)
+    // window.history.pushState({}, "", `?${newUrl}`);
+    navigate(`/projects${newUrl}`)
   }
-  
 });
 
-
 const createQueryString = (key: string, value: string) => {
-  
-  const currentParams = window.location.search
+  const currentParams = window.location.search;
   const params = new URLSearchParams(currentParams);
   const previousValues = params.get(key);
   const combinedValues = `${previousValues},${value}`;
-  
-  
+
   if (!previousValues) {
     params.set(key, value);
-    console.log("called: ", params.toString())
-    return params.toString();
+    return `?${params.toString()}`;
   }
-  
-  
-  if (previousValues.includes(value)) {
 
+  if (
+    previousValues.includes(value) &&
+    previousValues.split(",").includes(value)
+  ) {
     const combinedValueArray = combinedValues
       .split(",")
       .filter((item) => item !== value);
-    
-    
-    if (combinedValueArray.length) {
-      params.delete(key);
-      return params.toString();
-    }
-    params.set(key, combinedValues);
-    return params.toString();
-  }
-  
-  
-  params.set(key, combinedValues);
-  return params.toString();
-  
-};
 
+    if (!combinedValueArray.length) {
+      params.delete(key);
+      return `${params.toString()}`;
+    }
+
+    params.set(key, combinedValueArray.join(","));
+    return `?${params.toString()}`;
+  }
+
+  params.set(key, combinedValues);
+  return `?${params.toString()}`;
+};
