fa5c52dc403b232734fecd7600014837a93879cc
David Henery <david4henery00@hotmail.com>
2026-08-11 19:13:15 +0100

    added-Navigation-API-debug-check

--- src/js/searchParams.ts
+++ src/js/searchParams.ts
@@ -1,15 +1,18 @@
-// document.body.insertAdjacentHTML(
-//   "afterbegin",
-//   `<div style="
-//     position:fixed;
-//     top:0;
-//     left:0;
-//     z-index:99999;
-//     background:red;
-//     color:white;
-//     padding:20px;
-//   ">SCRIPT LOADED</div>`,
-// );
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `<div style="
+    position:fixed;
+    top:0;
+    left:0;
+    z-index:99999;
+    background:red;
+    color:white;
+    padding:20px;
+  ">
+    Navigation: ${"navigation" in window}<br>
+    Navigate: ${typeof window.navigation?.navigate}
+  </div>`,
+);
 
 const resetBtn = document.getElementById("reset-btn") as HTMLButtonElement;
 const paginationContainer = document.getElementById(
@@ -88,18 +91,6 @@ resetBtn?.addEventListener("click", () => {
 
 // Pagination
 paginationContainer?.addEventListener("click", (ev) => {
-  document.body.insertAdjacentHTML(
-    "afterbegin",
-    `<div style="
-      position:fixed;
-      top:0;
-      left:0;
-      z-index:99999;
-      background:green;
-      color:white;
-      padding:20px;
-    ">CLICK HANDLER WORKS</div>`,
-  );
   const evTarget = ev.target as HTMLElement;
   const btnElement = evTarget.closest("button");
   if (btnElement && btnElement.dataset?.pagenum) {
